1a49cb608d1077a8d29b52cba57d795182aa3b75
lorenspaez <54387407+lorenspaez@users.noreply.github.com>
2022-07-08 01:57:14 -0400

    created entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AlertModule } from './alert/alert.module';
+import { OrganizationModule } from './organization/organization.module';
+import { PostModule } from './post/post.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AlertModule, OrganizationModule, PostModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
