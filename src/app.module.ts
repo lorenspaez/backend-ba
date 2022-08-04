af3a7779967fec831cbf35fd03a0c8cd7cc88844
lorenspaez <54387407+lorenspaez@users.noreply.github.com>
2022-08-03 23:20:13 -0400

    set env varibles for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,20 @@ import { CategoryModule } from './category/category.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { MassiveModule } from '@nestjsplus/massive';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
+    MassiveModule.register({
+      user: process.env.POSTGRES_USERNAME || '<local-postgres-user>',
+      password: process.env.POSTGRES_PASSWORD || '<local-postgres-password>',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: 5432,
+      database: process.env.POSTGRES_DATABASE ||'<local-postgres-db-name>'
+      }),
     UserModule,
     OrganizationModule,
     AlertModule,
